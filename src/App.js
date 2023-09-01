b425cbe9d74811012e94c25cf826859f097a2387
Saurabh <saurabhjha1402@gmail.com>
2023-09-01 16:09:19 +0530

    landing and get started page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import './App.css';
+import   './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 // import UserRegistrationPage from './pages/UserRegistrationPage';
 // import IARegistrationPage from './pages/IARegistrationPage'
 import CertificatesPage from './components/individualUser/userCertificates/CertificatesPage';
-
+import LandingPage from './components/landingPage/LandingPage';
+import GetStarted from './components/landingPage/GetStarted';
 function App() {
   return (
     <BrowserRouter>
       {/* <Navbar /> */}
-        <Routes>
+        
           {/* <Route exact path="/" element={<UserRegistrationPage/>}/> */}
           {/* <Route exact path="/" element={<IARegistrationPage/>}/> */}
           {/* <Route exact path="/" element={<CertificatesPage/>}/> */}
-        </Routes>
-      <Footer />
+        <LandingPage/>
+        <GetStarted/>
+   
+   
     </BrowserRouter>
+    
   );
 }
 
